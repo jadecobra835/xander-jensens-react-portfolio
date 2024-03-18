3de11b19b9e34535de40477c6f1eda6f23d0e014
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-18 09:02:12 -0600

    Added all the portfolio items and images via API

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function(props) {
+    // Data that we'll need:
+    // - background image: thumb_image_url
+    // - logo: logo_url
+    // - description: description
+    // - id: id
+    // [ "id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images" ]
+
+    const { id, description, thumb_image_url, logo_url } = props.item;
     return (
         <div>
-            <h3>{props.title}</h3>
-            <h4>{props.url}</h4>
-            <Link to={`/portfolio/${props.slug}`}>Link</Link>
+            <img src={thumb_image_url} />
+            <img src={logo_url} />
+            <div>{description}</div>
+            <Link to={`/portfolio/${id}`}>Link</Link>
         </div>
     );
 }
