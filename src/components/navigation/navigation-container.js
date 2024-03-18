b5aa8b717a0fab1a39150f3bf44d3051b2f6d169
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-18 14:34:58 -0600

    Added basic navbar scss styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,13 +8,25 @@ export default class NavigationComponent extends Component {
     
     render() {
         return (
-            <div>
-                <NavLink exact to ="/" activeClassName="nav-link-active">Home</NavLink>
-                <NavLink to ="/about-me" activeClassName="nav-link-active">About</NavLink>
-                <NavLink exact to ="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                <NavLink exact to ="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                
-                {false ? <button>Add Blog</button> : null }
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to ="/" activeClassName="nav-link-active">Home</NavLink>
+                    </div>  
+                    <div className="nav-link-wrapper">
+                        <NavLink to ="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    </div>                 
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to ="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                    </div>                  
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to ="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                    </div>                   
+                </div>
+
+                <div className="right-side">
+                    XANDER JENSEN
+                </div>
             </div>
         );
     }
