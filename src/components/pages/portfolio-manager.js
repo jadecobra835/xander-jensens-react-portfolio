6b86e3dbd9ca7ede8232a7540de3de328af08c46
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-22 08:58:48 -0600

    Added the portfolio-item form

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
  
 export default class PortfolioManager extends Component {
     constructor() {
@@ -11,13 +12,26 @@ export default class PortfolioManager extends Component {
             isLoading: false,
             portfolioItems: []
         }
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    };
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        this.setState({
+            portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+        });
+    };
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmissionError error", error)
     };
 
 
 
     getPortfolioItems() {
         axios
-            .get('https://xanderjensen.devcamp.space/portfolio/portfolio_items', {withCredentials: true })
+            .get('https://xanderjensen.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc', {withCredentials: true })
             .then(response => {
                 this.setState({
                     portfolioItems: [...response.data.portfolio_items]
@@ -40,7 +54,10 @@ export default class PortfolioManager extends Component {
         return (
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
-                    <h1>Portfolio form....</h1>
+                    <PortfolioForm 
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+                    />
                 </div> 
 
                 <div className="right-column">
