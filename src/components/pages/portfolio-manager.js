2d50e41772380a5cf1a2aa14deb7c4541926668b
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-25 12:58:33 -0600

    Added the ability to Edit Portfolio Items

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -10,13 +10,29 @@ export default class PortfolioManager extends Component {
 
         this.state = {
             isLoading: false,
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {}
         }
 
-        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+        this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this)
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
+        this.clearPortfolioToEdit = this.clearPortfolioToEdit.bind(this);
     };
+
+    clearPortfolioToEdit() {
+        this.setState({
+            portfolioToEdit: {}
+        });
+    };
+
+    handleEditClick(portfolioItem) {
+        this.setState({
+            portfolioToEdit: portfolioItem
+        });
+    }; 
 
     handleDeleteClick(portfolioItem) {
         console.log("handleDeleteClick", portfolioItem);
@@ -36,7 +52,11 @@ export default class PortfolioManager extends Component {
         })
     };
 
-    handleSuccessfulFormSubmission(portfolioItem) {
+    handleEditFormSubmission() {
+        this.getPortfolioItems();
+    };
+
+    handleNewFormSubmission(portfolioItem) {
         this.setState({
             portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
         });
@@ -72,15 +92,19 @@ export default class PortfolioManager extends Component {
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
                     <PortfolioForm 
-                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleNewFormSubmission={this.handleNewFormSubmission}
+                        handleEditFormSubmission={this.handleEditFormSubmission}
                         handleFormSubmissionError={this.handleFormSubmissionError}
+                        clearPortfolioToEdit={this.clearPortfolioToEdit}
+                        portfolioToEdit={this.state.portfolioToEdit}
                     />
                 </div> 
 
                 <div className="right-column">
                     <PortfolioSidebarList 
                         handleDeleteClick={this.handleDeleteClick}
-                        data={this.state.portfolioItems} 
+                        data={this.state.portfolioItems}
+                        handleEditClick={this.handleEditClick} 
                     />
                 </div>   
             </div>
--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,6 +19,9 @@ export default class PortfolioForm extends Component {
             thumb_image: "",
             banner_image: "",
             logo: "",
+            editMode: false,
+            apiUrl: "https://xanderjensen.devcamp.space/portfolio/portfolio_items",
+            apiAction: 'post'
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -32,6 +35,37 @@ export default class PortfolioForm extends Component {
         this.thumbRef = React.createRef();
         this.bannerRef = React.createRef();
         this.logoRef = React.createRef();
+    };
+
+    // Lifecycle Hook (google it)
+    componentDidUpdate() {
+        if (Object.keys(this.props.portfolioToEdit).length > 0) {
+            const  {
+                id,
+                name,
+                description,
+                category,
+                position,
+                url,
+                thumb_image_url,
+                banner_image_url,
+                logo_url
+            } = this.props.portfolioToEdit;
+
+            this.props.clearPortfolioToEdit();
+
+            this.setState({
+                id: id,
+                name: name || "",
+                description: description || "",
+                category: category || "eCommerce",
+                position: position || "",
+                url: url || "",
+                editMode: true,
+                apiUrl: `https://xanderjensen.devcamp.space/portfolio/portfolio_items/${id}`,
+                apiAction: 'patch'
+            })
+        };
     };
 
     handleThumbDrop() {
@@ -99,12 +133,17 @@ export default class PortfolioForm extends Component {
     };
 
     handleSubmit(event) {
-        axios.post(
-            "https://xanderjensen.devcamp.space/portfolio/portfolio_items",
-            this.buildForm(), 
-            { withCredentials: true }
-        ).then(response => {
-            this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+        }).then(response => {
+            if (this.state.editMode) {
+                this.props.handleEditFormSubmission()
+            } else {
+                this.props.handleNewFormSubmission(response.data.portfolio_item)
+            }
 
             this.setState({
                 name: "",
@@ -114,7 +153,10 @@ export default class PortfolioForm extends Component {
                 url: "",
                 thumb_image: "",
                 banner_image: "",
-                logo: ""
+                logo: "",
+                editMode: false,
+                apiUrl: "https://xanderjensen.devcamp.space/portfolio/portfolio_items",
+                apiAction: 'post'
             });
 
             [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
