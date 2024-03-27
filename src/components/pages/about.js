b4b374b59eca411b387e92125f3fc0fa027aaa05
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-27 13:40:55 -0600

    Added about me template and styles

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import loginImg from '../../../static/assets/images/auth/login.jpg';
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div 
+                className="left-column"
+                style={{
+                    background: "url(" + loginImg + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}> 
+                
+            </div>
+            <div className="right-column">
+                <h1 className="title">About Me</h1>
+                <div className="content-wrapper">
+                    <p>My Bio</p>
+                </div>
+            </div>
         </div>
     );
 }
