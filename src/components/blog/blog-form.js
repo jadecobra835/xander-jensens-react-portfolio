b97f29e65f2ae9753f6e7ea8b7c1b7d84fae9418
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-27 11:41:29 -0600

    Finished modal

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -21,6 +21,8 @@ export default class BlogForm extends Component {
         this.componentConfig = this.componentConfig.bind(this);
         this.djsConfig = this.djsConfig.bind(this);
         this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+        this.featuredImageRef = React.createRef();
     };
 
     componentConfig() {
@@ -40,7 +42,7 @@ export default class BlogForm extends Component {
 
     handleFeaturedImageDrop() {
         return {
-            addfile: file => this.setState({ featured_image: file })
+            addedfile: file => this.setState({ featured_image: file })
         };
     };
 
@@ -58,19 +60,28 @@ export default class BlogForm extends Component {
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
         formData.append("portfolio_blog[content]", this.state.content);
 
+        if (this.state.featured_image) {
+            formData.append("portfolio_blog[featured_image]", this.state.featured_image);
+        };
+
         return formData;
     };
 
     handleSubmit(event) {
         axios.post(
             "https://xanderjensen.devcamp.space/portfolio/portfolio_blogs", 
-            this.buildForm(), 
+            this.buildForm(),
             { withCredentials: true }
         ).then(response => {
+            if (this.state.featured_image) {
+                this.featuredImageRef.current.dropzone.removeAllFiles();
+            }
+            
             this.setState({
                 title: "",
                 blog_status: "",
-                content: ""
+                content: "",
+                featured_image: ""
             });
 
             this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
@@ -114,6 +125,7 @@ export default class BlogForm extends Component {
 
                 <div className="image-uploaders">
                     <DropzoneComponent 
+                        ref={this.featuredImageRef}
                         config={this.componentConfig()}
                         djsConfig={this.djsConfig()}
                         eventHandlers={this.handleFeaturedImageDrop()}
@@ -121,7 +133,7 @@ export default class BlogForm extends Component {
                         <div className="dz-message">Featured Image</div>
                     </DropzoneComponent>
                 </div>
-
+                
                 <button className="btn">Save</button>
             </form>
         );
