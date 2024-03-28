d07e0627b77e57b44c0d240f56198479c62f0a10
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-28 09:10:33 -0600

    Added ability to delete blogs

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -23,9 +23,28 @@ class Blog extends Component {
         this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
         this.handleModalClose = this.handleModalClose.bind(this);
         this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
 
         window.addEventListener("scroll", this.onScroll, false);
     };
+
+    handleDeleteClick(blog) {
+        axios.delete(
+            `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+            { withCredentials: true}
+        ).then(response => {
+            this.setState({
+                blogItems: this.state.blogItems.filter(blogItem => {
+                    return blog.id != blogItem.id
+                })
+
+            });
+
+            return response.data;
+        }).catch(error => {
+            console.log("handleDeleteClick error", error)
+        })
+    }
 
     handleSuccessfulNewBlogSubmission(blog) {
         this.setState({
@@ -86,7 +105,18 @@ class Blog extends Component {
     
     render() {
         const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+            if (this.props.loggedInStatus === "LOGGED_IN") {
+                return (
+                    <div key={blogItem.id} className="admin-blog-wrapper">
+                        <BlogItem blogItem={blogItem} />
+                        <a onClick={() => this.handleDeleteClick(blogItem)}>
+                            <FontAwesomeIcon icon="trash" />
+                        </a>
+                    </div>
+                )
+            } else {
+                return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+            }
         })
 
         return (
